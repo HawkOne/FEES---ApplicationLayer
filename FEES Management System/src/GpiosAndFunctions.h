e959597bb1096b61b8a5010fb45b07e03ad7c183
HawkOne <stefano.ampolo@gmail.com>
2020-05-01 19:12:34 +0200

    [Commit] Rischioso, non compila
    Faccio il commit per capire se e' la mia workstation a non funzionare o no.

--- FEES Management System/src/GpiosAndFunctions.h
+++ FEES Management System/src/GpiosAndFunctions.h
@@ -18,16 +18,15 @@
  *      F::::::::FF           E::::::::::::::::::::EE::::::::::::::::::::ES:::::::::::::::SS       *
  *      FFFFFFFFFFF           EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE SSSSSSSSSSSSSSS         *
  *												   												   *
- *               ____    ___     __      ___    ___    __  ___ __       ___   ___        	   	   *
- *               |__ |   |__ \_/||__)|   |__    |__ \_/|__)|__ |__)||\/||__ |\ || /\ |    	       *
- *               |   |___|___/ \||__)|___|___   |___/ \|   |___|  \||  ||___| \||/~~\|___ 	       *
- *                         ___     __  ___ __  __  ___ __     __     ________         		       *
- *                        |__ |\/||__)|__ |  \|  \|__ |  \   /__`\ //__`||__ |\/|     		       *
- *                        |___|  ||__)|___|__/|__/|___|__/   .__/ | .__/||___|  |     		       *
- *                                                                                       	       *
+ *                  __    __     __     __   __    __  __ __       __    ___                       *
+ *                 |_ |  |_ \_/||__)|  |_   |_ \_/|__)|_ |__)||\/||_ |\ | |  /\ |                  *
+ *                 |  |__|__/ \||__)|__|__  |__/ \|   |__| \ ||  ||__| \| | /--\|__                *
+ *                    __     __  __ __  __  __ __    __   ___ __       ___ __                      *
+ *                   |_ |\/||__)|_ |  \|  \|_ |  \  (_  /\ | |_ |  |  | | |_                       *
+ *                   |__|  ||__)|__|__/|__/|__|__/  __)/--\| |__|__|__| | |__                      *
+ *                                                                                                 *
  *												    											   *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *												   												   *
  *												   												   *
  *											FEES Project			 					           *
  *		    			URL: https://github.com/ferrandi/DEIB-DAER-project		                   *
@@ -36,20 +35,20 @@
  *		 			  ***********************************************************                  *
  *        		 	   		Copyright (c) 2018-2019 Politecnico di Milano				   		   *
  *                                                                                                 *
- *   This file is part of the FEES framework.                                                      *
+ *     This file is part of the FEES framework.                                                    *
  *                                                                                                 *
- *   The FEES framework is free software; you can redistribute it and/or modify                    *
- *   it under the terms of the GNU General Public License as published by                          *
- *   the Free Software Foundation; either version 3 of the License, or                             *
- *   (at your option) any later version.                                                           *
+ *     The FEES framework is free software; you can redistribute it and/or modify                  *
+ *     it under the terms of the GNU General Public License as published by                        *
+ *     the Free Software Foundation; either version 3 of the License, or                           *
+ *     (at your option) any later version.                                                         *
  *                                                                                                 *
- *   This program is distributed in the hope that it will be useful,                           	   *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of                                *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                                 *
- *   GNU General Public License for more details.                                                  *
+ *     This program is distributed in the hope that it will be useful,                             *
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of                              *
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                               *
+ *     GNU General Public License for more details.                                                *
  *                                                                                                 *
- *   You should have received a copy of the GNU General Public License                             *
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>.                         *
+ *     You should have received a copy of the GNU General Public License                           *
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.                       *
  *                                                                                                 *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  *
@@ -146,10 +145,12 @@ void BatteryThermal_Management_ON();
 void BatteryThermal_Management_OFF();
 
 /**********| WatchDog  Control 	**********************		******/
-void Hardware_Watchdog(); // Questa funzione fa partire il Thread che gestisce il Watchdog Hardware
+void Hardware_Watchdog_ON(); // Questa funzione fa partire il Thread che gestisce il Watchdog Hardware
+void Hardware_Watchdog_OFF();
 
 /**********| LED - TELECOM WATCHDOG  Control  (GPIO 19) *** ******/
-void Transmission_Watchdog(); // Questa funzione fa partire il Thread che gestisce Led-Watchdog
+void Transmission_Watchdog_ON(); // Questa funzione fa partire il Thread che gestisce Led-Watchdog
+void Transmission_Watchdog_OFF();
 
 // Thread for FiniteStateMachine Handling
 void thread_Fsm(); // 1Hz Thread loop
